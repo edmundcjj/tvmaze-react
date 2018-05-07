b8602c6c1a7ecdbb4f0eb9ee69b56d7338fd244b
Edmund <edmundcjj@gmail.com>
2018-05-08 00:54:30 +0800

    Query and display related movie titles

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -7,23 +7,7 @@ import Home from './components/home/main';
 
 const RESULTS = require('../../results.js');
 
-// Search Bar
-class SearchBar extends React.Component {
-  constructor(){
-    super()
-  }
-
-  render() {
-    return (
-      <form className={styles.center_form}>
-        <input type="text" placeholder="Search..." />
-        <button onClick={this.search}>Search</button>
-      </form>
-    );
-  }
-}
-
-// Individual Result
+// Each Individual Result
 class Result_item extends React.Component {
   constructor(){
     super()
@@ -36,19 +20,62 @@ class Result_item extends React.Component {
           <h3>{this.props.name}</h3>
           <p>Language: {this.props.language}</p>
           <p>Rating: {this.props.rating}</p>
+          <a href={this.props.show_url} target="_blank">Link to movie</a>
           <hr></hr>
         </div>
       );
   }
 }
 
+// List of results after querying
 class Result_List extends React.Component {
   constructor(){
-    super()
+    super();
+
+    this.state = {
+      query: "",
+      movie_list: [],
+      queried_movie_list: []
+    }
+
+    this.populate_movie_list = this.populate_movie_list.bind( this );
+    this.search = this.search.bind( this );
+    this.set_query = this.set_query.bind( this );
+  }
+
+  // Save all the movies retrieved from the json file into an array
+  populate_movie_list(){
+    this.props.results.results.map( (result) => {
+      this.state.movie_list.push(result);
+    });
+    this.setState({movie_list: this.state.movie_list});
+  }
+
+  // Save the query
+  set_query(event){
+    this.setState({query: event.target.value});
+  }
+
+  // Function to retrieve the related movie titles based on the query
+  search(){
+    this.populate_movie_list();
+
+    let current_query = this.state.query;
+    let movies_array = this.state.movie_list;
+
+    this.setState({query: ""});
+
+    for (var i = 0; i < movies_array.length; i++) {
+      if (movies_array[i].show.name.indexOf(current_query) != -1) {
+        this.state.queried_movie_list.push(movies_array[i]);
+      }
+    }
+    this.setState({queried_movie_list: this.state.queried_movie_list});
   }
 
   render() {
-    const ResultNode = this.props.results.results.map( (result) => {
+    console.log("Movie List =>", this.state.queried_movie_list);
+    const ResultNode = this.state.queried_movie_list.map( (result) => {
                           return <Result_item key={result.show.id}
                                               name={result.show.name}
                                               rating={result.show.rating.average}
@@ -59,16 +86,23 @@ class Result_List extends React.Component {
     });
 
     return (
-      <div>{ResultNode}</div>
+      <div>
+        <div className={styles.center_form}>
+          <input type="text" placeholder="Search..." onChange={this.set_query} value={this.state.query}/>
+          <button onClick={this.search}>Search</button>
+        </div>
+
+        <br></br>
+
+        {ResultNode}
+      </div>
     );
   }
 }
 
 const App = () => (
   <div>
-    <Home />,
-
-    <SearchBar />,
+    <Home />
 
     <Result_List results={RESULTS} />
   </div>
